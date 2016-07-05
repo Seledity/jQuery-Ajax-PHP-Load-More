c5bc9b6c3352fe3a020e83a294c59d3b09ec0a69
Justin Salyers <me@justa.us>
2016-07-05 16:39:46 -0400

    Update ajax_more.php

--- ajax_more.php
+++ ajax_more.php
@@ -1,7 +1,7 @@
 <?php
-require_once("../inc/conx.php");
+require_once("../inc/conx.php"); //change this to your connection file or info
 $amount = $_GET['amntcnt'];
-$contentq = mysqli_query($conx, "SELECT id,post FROM feed LIMIT $amount,3");
+$contentq = mysqli_query($conx, "SELECT id,post FROM feed LIMIT $amount,3"); //change this to what you're grabbing from the database
 while($contentr = mysqli_fetch_assoc($contentq)) {
   $cnt_id = $contentr['id'];
   $cnt_txt = $contentr['post'];
