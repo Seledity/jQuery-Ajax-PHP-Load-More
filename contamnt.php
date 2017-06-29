ebbdd1a1f41ee4d4e72eb8046426517af8f306de
Justin Salyers <me@justa.us>
2017-06-28 22:26:04 -0400

    Update contamnt.php

--- contamnt.php
+++ contamnt.php
@@ -1,4 +1,4 @@
 <?php
 require_once("../inc/conx.php"); //change this to your connection file or info
-echo mysqli_num_rows(mysqli_query($conx, "SELECT * FROM feed")); //change this to the table you're grabbing from the database
+echo mysqli_num_rows(mysqli_query($conx, "SELECT id FROM feed")); //change this to the table you're grabbing from the database
 ?>
